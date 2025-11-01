56feae848fbd557ae98320ce898478ee84b0b45a
Matt <medangina@gmail.com>
2025-11-01 14:17:12 -0600

    NPS Part 5

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,21 @@
-//Import functions from other mjs files
-import { getParkData, parkInfoLinks, getInfoLinks } from "./parkService.mjs";
+import "../css/style.css"; // we can do this because we are using Vite...
+import "../css/home.css";
+import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
 
-//Function to update the intro of a page
 function setParkIntro(data) {
-    //Get first element in .intro class
-    const introEl = document.querySelector(".intro");     
-    //Replace inside of .intro with new park name (h1) and desc (p)
-    introEl.innerHTML = `<h1>${parkData.fullName}</h1>
-    <p>${parkData.description}</p>`;
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${data.fullName}</h1>
+  <p>${data.description}</p>`;
 }
 
-//Function to update page links and info
 function setParkInfoLinks(data) {
-    //Get first element in .info again
-    const infoEl = document.querySelector(".info");
-    //Get data array and run through template function
-    const html = data.map(mediaCardTemplate);
-    //Take array and join into one HTML string and insert at start of .info
-    infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+  const infoEl = document.querySelector(".info");
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
+  const html = data.map(mediaCardTemplate);
+  // join the array of strings into one string and insert it into the section
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
 async function init() {
@@ -30,5 +26,32 @@ async function init() {
   setParkInfoLinks(links);
 }
 
-//Call functions
+function enableNavigation() {
+  const menuButton = document.querySelector("#global-nav-toggle");
+  const subMenuToggles = document.querySelectorAll(
+    ".global-nav__split-button__toggle"
+  );
+  // when the main menu button is clicked:
+  menuButton.addEventListener("click", (ev) => {
+    let target = ev.target;
+    // toggle the show class on the global-nav
+    document.querySelector(".global-nav").classList.toggle("show");
+    // check to see if ev.target is the button or something inside the button
+    if (target.tagName != "BUTTON") {
+      target = target.closest("button");
+    }
+    // check to see if we just opened or closed the menu
+    if (document.querySelector(".global-nav").classList.contains("show")) {
+      // if we opened it then set the aria-expanded attribute to true
+      target.setAttribute("aria-expanded", true);
+    } else {
+      // if we closed it then set the aria-expanded attribute to false
+      target.setAttribute("aria-expanded", false);
+    }
+
+    console.log("toggle");
+  });
+}
+
 init();
+enableNavigation();
